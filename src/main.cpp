f4b6ca3f77daa9f6763d209c706ac868999d5a8b
Utsav254 <u15goel@outlook.com>
2025-04-01 19:28:56 +0100

    test text feature

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,39 @@
 #include "WinTerm/winTerm.hpp"
+#include <string>
+
+std::string buffer;
 
 void termProc(handle<winTerm::msg> msg) {
 	switch (msg->m) {
 		case winTerm::message::KEYBOARD:
-			switch(std::get<winTerm::keyboard>(msg->param))
 			{
-				case winTerm::keyboard::CTRL_Q:
-					winTerm::postQuitMessage(0);
-					break;
-				default:
+				winTerm::keyboard kbd = std::get<winTerm::keyboard>(msg->param);
+				if(kbd >= 32 && kbd < 127) {
+					if(buffer.size() % 94 == 0 && buffer.size() != 0){
+						buffer += '\n';
+					}
+					buffer += char(kbd);
 					winTerm::postPaintMessage();
-					break;
+				}
+				else if(kbd == winTerm::keyboard::CTRL_Q) {
+					winTerm::postQuitMessage(0);
+				}
 			}
 			break;
 		case winTerm::message::PAINT:
 			{
 				const unsigned int height = 35, width = 100;
 
-				auto cv = winTerm::beginPaint(width , height);
+				handle<winTerm::canvas> cv = winTerm::beginPaint(width , height);
 				cv->setBackground(winTerm::colour::blue);
 				cv->setBorder(winTerm::borderStyle::two);
 
 				cv->addText(" Window Tittle Here " , 0 , width / 2 - 21 / 2 , winTerm::colour::white,
 					winTerm::colour::red, winTerm::emphasis::bold);
 
-				cv->drawRect(winTerm::rect(2 , 2 , 97 , 20), winTerm::colour::green, static_cast<winTerm::borderStyle>(0) , false);
+				cv->drawRect(winTerm::rect(2, 2, 97, 20), winTerm::colour::green, static_cast<winTerm::borderStyle>(0), false);
+
+				cv->addText(buffer, 3, 3, winTerm::colour::white, winTerm::colour::green, winTerm::emphasis::norm);
 
 				winTerm::endPaint(std::move(cv));
 			}
@@ -32,8 +41,8 @@ void termProc(handle<winTerm::msg> msg) {
 		case winTerm::message::RESIZE:
 		case winTerm::message::NONE:
 		case winTerm::message::QUIT:
-          	break;
-        }
+			break;
+    }
 }
 
 int main()
@@ -49,7 +58,7 @@ int main()
 	}
 
 	winTerm::destroy();
-	
+
 	if(getEventResult == 0) return static_cast<int>(std::get<long>(msg->param));
 	else return EXIT_FAILURE;
 }
